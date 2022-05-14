e6e68a29e0c40566a6c666697e96ae1cedd649ec
Dave Stewart <dave.c.stewart@gmail.com>
2022-05-13 21:10:18 -0400

    asdf

---
+++ model.js
@@ -0,0 +1,32 @@
+let cats = [
+    {
+        name: "Home",
+        todos: [
+            {
+                desc: "Cut Grass",
+                due: "May 24",
+                isDone: false
+            },
+            {
+                desc: "Rake Leaves",
+                due: "May 24",
+                isDone: true
+            }
+        ]
+    },
+    {
+        name: "Work",
+        todos: [
+            {
+                desc: "Mark Grade 12 Assignments",
+                due: "ASAP",
+                isDone: false
+            },
+            {
+                desc: "Prepare Project",
+                due: "May 18",
+                isDone: false
+            }
+        ]
+    }
+]
